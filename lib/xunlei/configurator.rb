5819f0af4b78d545dc4cbcae70c420be064ca5fb
Forrest Ye <afu@forresty.com>
2012-02-09 18:09:32 +0800

    refactor: separate common configs from xunlei executable

---
+++ lib/xunlei/configurator.rb
@@ -0,0 +1,47 @@
+module Xunlei
+  module Configurator
+
+    def xunlei_folder_name
+      "~/.xunlei"
+    end
+
+    def xunlei_folder_path
+      File.expand_path(xunlei_folder_name)
+    end
+
+    def xunlei_folder_exists?
+      Dir.exists?(xunlei_folder_path)
+    end
+
+    def credential_file_exists?
+      File.exists?(xunlei_credential_file_path)
+    end
+
+    def create_xunlei_folder
+      Dir.mkdir(xunlei_folder_path)
+    end
+
+    def xunlei_cookies_path
+      File.join(xunlei_folder_path, "cookies.txt")
+    end
+
+    def xunlei_tasks_path
+      File.join(xunlei_folder_path, "all_tasks.yml")
+    end
+
+    def xunlei_credential_file_path
+      File.join(xunlei_folder_path, "credentials.yml")
+    end
+
+    def xunlei_downloaded_tasks_path
+      File.join(xunlei_folder_path, "downloaded.yml")
+    end
+
+    def chromedriver_zip_name
+      "chromedriver_mac.zip"
+    end
+
+  end
+
+  Config = Configurator
+end
