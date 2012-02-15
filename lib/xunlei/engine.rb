53b7c50038ede7b9518e217a5a207b9fdf653055
Forrest Ye <afu@forresty.com>
2012-02-15 23:09:28 +0800

    remove trailing whitespaces in engine.rb

--- lib/xunlei/engine.rb
+++ lib/xunlei/engine.rb
@@ -9,15 +9,15 @@ module Xunlei
       @browser.text_field(:id => "u").when_present.set(username)
       @browser.text_field(:id => "p_show").when_present.set(password)
       @browser.button(:id => "button_submit4reg").when_present.click
-      
+
       # temp fix for stupid cloud VOD popup
       wait_until_all_loaded
       @browser.div(:class => "p_rw_pop p_sc_pop p_yuntips").p(:class => 'p_btm_aline').as.first.when_present.click
     end
-    
+
     def dump_cookies
       wait_until_all_loaded
-      
+
       cookies = []
       @browser.driver.manage.all_cookies.each do |cookie|
         domain = cookie[:domain]
@@ -27,31 +27,31 @@ module Xunlei
         value = cookie[:value]
         cookies << "#{domain}\tTRUE\t#{path}\tFALSE\t#{expires}\t#{name}\t#{value}\n"
       end
-      
+
       cookies
     end
-    
+
     def dump_tasks
       all_files = []
-      
+
       begin
         all_files += process_current_page
       end while next_page
-      
+
       all_files
     end
-    
+
     def add_task(target_address)
       puts "Adding new task..."
       # open('|pbcopy', 'w') { |io| io << target_address }
-      
+
       @browser.execute_script("add_task_new(0)")
-      
+
       @browser.text_field(:id => 'task_url').wait_until_present
 
       # @browser.send_keys [:command, 'v']
       @browser.execute_script("document.getElementById('task_url').value = '#{target_address}'")
-      
+
       puts "Task URL = \"#{target_address}\""
       expire_count = 0
       sleep(2.0)
@@ -66,44 +66,44 @@ module Xunlei
         puts "Timed out, the button is unavailable."
       end
     end
-    
+
     def stop
       @browser.close
     end
-    
+
   private
-    
+
     def wait_until_all_loaded
       get_task_list
     end
-    
+
     def get_task_list
       task_list = @browser.div(:id => "rowbox_list")
       task_list.wait_until_present
       task_list
     end
-    
+
     def next_page
       next_li = @browser.li(:class => "next")
-      
+
       return false unless next_li.present?
-      
+
       next_li.as.first.click
     end
-    
+
     def process_current_page
       get_task_list.divs(:class => "rw_inter").inject([]) do |all_files, task_div|
         all_files += process_task(task_div)
       end
     end
-    
+
     def process_task(task_div)
       task_files = []
-      
+
       task_div.wait_until_present
-      
+
       @browser.execute_script("document.getElementById('#{task_div.parent.id}').scrollIntoView()")
-      
+
       if task_is_ready?(task_div)
         # puts "task is ready"
         task_div.click
@@ -117,28 +117,28 @@ module Xunlei
       else
         # still downloading
       end
-      
+
       task_files
     end
-    
+
     def task_is_ready?(task_div)
       task_div.em(:class => "loadnum").text == "100%"
     end
-    
+
     def process_normal_task(task_div)
       normal_task_a = task_div.span(:class => "namelink").as.first
       task_id = task_div.parent.id.gsub(/\D+/, "")
       normal_task_input = task_div.input(:id => "dl_url" + task_id)
       { :name => normal_task_a.text.gsub(/'|\\/,""), :url => normal_task_input.value, :size => task_div.span(:id => "size#{task_id}").text }
     end
-    
+
     def process_bt_task(task_div)
       task_files = []
       task_div.a(:class => "rwbtn ic_open").when_present.click
 
       folder_list = @browser.div(:id => "rwbox_bt_list")
       folder_list.wait_until_present
-      
+
       index = 0
       folder_list.spans(:class => "namelink").each do |span|
         s = span.spans.first
@@ -148,10 +148,10 @@ module Xunlei
       end
 
       go_back_from_bt_task
-      
+
       task_files
     end
-    
+
     def go_back_from_bt_task
       back_div = @browser.div(:id => "view_bt_list_nav")
       back_div.wait_until_present
